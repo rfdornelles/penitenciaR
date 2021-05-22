dc58c76f0fcbcd87547facefce4836da4f75c66c
Rodrigo Dornelles <rodornelles@gmail.com>
2021-05-22 19:22:18 -0300

    Função auxiliar renomeia_coluna para facilitar o fluxo de limpeza

---
+++ R/auxiliares.R
@@ -0,0 +1,24 @@
+#' Renomeia colunas
+#'
+#' @description
+#'
+#' Aplica a função dplyr::rename() à uma base, mudando o nome da coluna indicada.
+#' Será aplicada a função dplyr::matches() para identificar a coluna.
+#'
+#'Ao final, a coluna nova será
+#' @param base Tibble da base
+#' @param nome Nome que será recebido
+#' @param coluna Expressão que identificará a coluna
+#' @param fixa Por padrão, "cod_ibge". Identifica após qual coluna a nova será
+#' recolocada.
+#'
+#' @return Tibble renomeada
+#' @export
+#'
+
+renomeia_coluna <- function (base, nome, coluna, fixa = "cod_ibge") {
+  base %>%
+    dplyr::rename({{nome}} := dplyr::matches(match = coluna,
+                                             ignore.case = TRUE)) %>%
+    dplyr::relocate({{nome}}, .after = {{fixa}})
+}
