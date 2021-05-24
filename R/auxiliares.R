16d0af3cdce5691e41b515c8efa1967d2bf0b73c
Rodrigo Dornelles <rodornelles@gmail.com>
2021-05-23 21:14:46 -0300

    Tentativa de função para checar a consistência: comparar as colunas que tem total no nome com as que não

--- R/auxiliares.R
+++ R/auxiliares.R
@@ -23,3 +23,37 @@ renomeia_coluna <- function (base, nome, coluna, fixa = "cod_ibge") {
                                              ignore.case = TRUE)) %>%
     dplyr::relocate({{nome}}, .after = {{fixa}})
 }
+
+
+# checa_consistência ------------------------------------------------------
+
+
+#' Chaca consistência
+#'
+#' @description
+#'
+#' Testa a consistência das colunas: vai pegar as que não tem total, somar
+#' e comparar com a que tem.
+#'
+#' @param base Qual a base
+#' @param nome_coluna Nome da coluna "
+#'
+#' @return
+#' @export
+#'
+#'
+
+checa_consistencia_numero <- function(base, nome_coluna) {
+
+  base %>%
+    dplyr::select(
+      dplyr::contains(nome_coluna)) %>%
+    dplyr::mutate(
+      sem_total = rowSums(
+        dplyr::across(
+          dplyr::matches(match = "[^(Total)]")
+        ), na.rm = TRUE),
+      total = rowSums(dplyr::across(dplyr::contains("Total")), na.rm = TRUE),
+      consistente = sem_total == total
+    )
+}
