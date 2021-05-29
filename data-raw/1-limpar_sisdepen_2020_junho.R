fd6d145a841bcd58e455d0ea50b6050cc753fd78
Rodrigo Dornelles <rodornelles@gmail.com>
2021-05-29 01:28:11 -0300

    Substitui cap_ por capacidade_ e qnt_ por quantidade_

--- data-raw/1-limpar_sisdepen_2020_junho.R
+++ data-raw/1-limpar_sisdepen_2020_junho.R
@@ -18,6 +18,7 @@ lista_arquivos <- fs::dir_info(path = "data-raw/sisdepen/")$path
 # Carregar a base ---------------------------------------------------------
 path_base <- lista_arquivos[11] # seleciona junho 2020
 
+# dá ruim: 7, 2, 1
 base_sisdepen <- readxl::read_excel(path_base)
 
 # Vou selecionar como chaves:
@@ -51,24 +52,24 @@ base_renomeada <- base %>%
   renomeia_coluna("tipo_gestao", "1.4 Gestão do estabelecimento") %>%
   renomeia_coluna("regimento_interno_possui", "1.8 Possui regimento") %>%
   renomeia_coluna("regimento_interno_exclusivo", "1.9 O regimento interno") %>%
-  renomeia_coluna("cap_provisorio_mas", "1.3 .* provisórios \\| Masculino") %>%
-  renomeia_coluna("cap_provisorio_fem", "1.3 .* provisórios \\| Feminino") %>%
-  renomeia_coluna("cap_rfechado_mas", "1.3 .* fechado \\| Masculino") %>%
-  renomeia_coluna("cap_rfechado_fem", "1.3 .* fechado \\| Feminino") %>%
-  renomeia_coluna("cap_semiaberto_mas", "1.3 .* semiaberto \\| Masculino") %>%
-  renomeia_coluna("cap_semiaberto_fem", "1.3 .* semiaberto \\| Feminino") %>%
-  renomeia_coluna("cap_rdd_mas", "1.3 .* disciplinar .* \\| Masculino") %>%
-  renomeia_coluna("cap_rdd_fem", "1.3 .* disciplinar .* \\| Feminino") %>%
-  renomeia_coluna("cap_mseg_mas", "1.3 .* medidas de seg.* \\| Masculino") %>%
-  renomeia_coluna("cap_mseg_fem", "1.3 .* medidas de seg.* \\| Feminino") %>%
-  renomeia_coluna("cap_aberto_mas", "1.3 .* Regime aberto.* \\| Masculino") %>%
-  renomeia_coluna("cap_aberto_fem", "1.3 .* Regime aberto.* \\| Feminino") %>%
+  renomeia_coluna("capacidade_provisorio_mas", "1.3 .* provisórios \\| Masculino") %>%
+  renomeia_coluna("capacidade_provisorio_fem", "1.3 .* provisórios \\| Feminino") %>%
+  renomeia_coluna("capacidade_rfechado_mas", "1.3 .* fechado \\| Masculino") %>%
+  renomeia_coluna("capacidade_rfechado_fem", "1.3 .* fechado \\| Feminino") %>%
+  renomeia_coluna("capacidade_semiaberto_mas", "1.3 .* semiaberto \\| Masculino") %>%
+  renomeia_coluna("capacidade_semiaberto_fem", "1.3 .* semiaberto \\| Feminino") %>%
+  renomeia_coluna("capacidade_rdd_mas", "1.3 .* disciplinar .* \\| Masculino") %>%
+  renomeia_coluna("capacidade_rdd_fem", "1.3 .* disciplinar .* \\| Feminino") %>%
+  renomeia_coluna("capacidade_mseg_mas", "1.3 .* medidas de seg.* \\| Masculino") %>%
+  renomeia_coluna("capacidade_mseg_fem", "1.3 .* medidas de seg.* \\| Feminino") %>%
+  renomeia_coluna("capacidade_aberto_mas", "1.3 .* Regime aberto.* \\| Masculino") %>%
+  renomeia_coluna("capacidade_aberto_fem", "1.3 .* Regime aberto.* \\| Feminino") %>%
   renomeia_coluna("tipo_capacidade_outros", "1.3 Capacidade .* (Qual\\(is\\)\\?)$") %>%
-  renomeia_coluna("cap_outro_mas", "1.3 Capacidade .* Outro.* \\| Masculino") %>%
-  renomeia_coluna("cap_outro_fem", "1.3 Capacidade .* Outro.* \\| Feminino") %>%
-  renomeia_coluna("qnt_celas_interdidatas", "1.3 .* \\| Celas não aptas") %>%
-  renomeia_coluna("cap_interditada_mas", "1.3 .* desativadas .* Masculino") %>%
-  renomeia_coluna("cap_interditada_fem", "1.3 .* desativadas .* Feminino") %>%
+  renomeia_coluna("capacidade_outro_mas", "1.3 Capacidade .* Outro.* \\| Masculino") %>%
+  renomeia_coluna("capacidade_outro_fem", "1.3 Capacidade .* Outro.* \\| Feminino") %>%
+  renomeia_coluna("quantidade_celas_interdidatas", "1.3 .* \\| Celas não aptas") %>%
+  renomeia_coluna("capacidade_interditada_mas", "1.3 .* desativadas .* Masculino") %>%
+  renomeia_coluna("capacidade_interditada_fem", "1.3 .* desativadas .* Feminino") %>%
   # ver com cuidado essa: terceiriza_nenhum deveria ser não para todos os demais
   renomeia_coluna("terceiriza_nenhum", "1.5 .* Nenhum") %>%
   renomeia_coluna("terceiriza_alimentacao", "1.5 .* Alimentação") %>%
@@ -87,5 +88,5 @@ base_renomeada %>%
   View()
 
 #TODO: comparar as colunas que sobraram pra conferir a confiabilidade
-#TODO: checar se rola fazer para os outros anos
+#TODO: checar se rola fazer para os outros anos -> rola!
 
