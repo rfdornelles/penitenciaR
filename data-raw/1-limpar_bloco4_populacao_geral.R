e21f83f43f834323d08ee20ffe5f5c1615a644da
Rodrigo Dornelles <rodornelles@gmail.com>
2021-05-29 13:57:08 -0300

    Bloco 4 - informações gerais sobre população carcerária

--- data-raw/1-limpar_bloco4_populacao_geral.R
+++ data-raw/1-limpar_bloco4_populacao_geral.R
@@ -37,17 +37,66 @@ sisdepen2020 <- base_sisdepen %>%
 
 # pegar as variáveis do bloco 4
 base <- sisdepen2020 %>%
-  dplyr::select(estabelecimento_nm:cod_ibge,
+  dplyr::select(estabelecimento_nm, uf, cod_ibge,
                 dplyr::matches(match = "^[4]\\."))
 
-names(base)
+# stringr::str_c(names(base), collapse = "\n") %>% cat()
 
 
 # Renomear colunas --------------------------------------------------------
 
-base_renomeada <- base %>%
-  renomeia_coluna("data_inauguracao", "1.6 Data de inauguração") %>%
-  renomeia_coluna("terceiriza_quais_outros", "1.5 .* Outro")
+# base_renomeada <-
+base %>%
+  # provisórios
+  renomeia_coluna("quantidade_presoprovisorio_jusestadual_mas", "4.1 .*provisórios.*Estadual Masc") %>%
+  renomeia_coluna("quantidade_presoprovisorio_jusestadual_fem", "4.1 .*provisórios.*Estadual Femin") %>%
+  renomeia_coluna("quantidade_presoprovisorio_jusfederal_mas", "4.1 .*provisórios.*Federal Masc") %>%
+  renomeia_coluna("quantidade_presoprovisorio_jusfederal_fem", "4.1 .*provisórios.*Federal Femin") %>%
+  renomeia_coluna("quantidade_presoprovisorio_outrajus_mas", "4.1 .*provisórios.*Outros.* Masc") %>%
+  renomeia_coluna("quantidade_presoprovisorio_outrajus_fem", "4.1 .*provisórios.*Outros.* Femin") %>%
+  # presos sentenciados - regime fechado
+  renomeia_coluna("quantidade_sentenca_rf_jusestadual_mas", "4.1 .*sentenciados.*fechado.*Estadual Masc") %>%
+  renomeia_coluna("quantidade_sentenca_rf_jusestadual_fem", "4.1 .*sentenciados.*fechado.*Estadual Femin") %>%
+  renomeia_coluna("quantidade_sentenca_rf_jusfederal_mas", "4.1 .*sentenciados.*fechado.*Federal Masc") %>%
+  renomeia_coluna("quantidade_sentenca_rf_jusfederal_fem", "4.1 .*sentenciados.*fechado.*Federal Femin") %>%
+  renomeia_coluna("quantidade_sentenca_rf_outrajus_mas", "4.1 .*sentenciados.*fechado.*Outros.* Masc") %>%
+  renomeia_coluna("quantidade_sentenca_rf_outrajus_fem", "4.1 .*sentenciados.*fechado.*Outros.* Femin") %>%
+  # presos sentenciados - regime semi aberto
+  renomeia_coluna("quantidade_sentenca_rsa_jusestadual_mas", "4.1 .*sentenciados.*semiaberto.*Estadual Masc") %>%
+  renomeia_coluna("quantidade_sentenca_rsa_jusestadual_fem", "4.1 .*sentenciados.*semiaberto.*Estadual Femin") %>%
+  renomeia_coluna("quantidade_sentenca_rsa_jusfederal_mas", "4.1 .*sentenciados.*semiaberto.*Federal Masc") %>%
+  renomeia_coluna("quantidade_sentenca_rsa_jusfederal_fem", "4.1 .*sentenciados.*semiaberto.*Federal Femin") %>%
+  renomeia_coluna("quantidade_sentenca_rsa_outrajus_mas", "4.1 .*sentenciados.*semiaberto.*Outros.* Masc") %>%
+  renomeia_coluna("quantidade_sentenca_rsa_outrajus_fem", "4.1 .*sentenciados.*semiaberto.*Outros.* Femin") %>%
+  # presos sentenciados - regime aberto
+  renomeia_coluna("quantidade_sentenca_ra_jusestadual_mas", "4.1 .*sentenciados.* aberto.*Estadual Masc") %>%
+  renomeia_coluna("quantidade_sentenca_ra_jusestadual_fem", "4.1 .*sentenciados.* aberto.*Estadual Femin") %>%
+  renomeia_coluna("quantidade_sentenca_ra_jusfederal_mas", "4.1 .*sentenciados.* aberto.*Federal Masc") %>%
+  renomeia_coluna("quantidade_sentenca_ra_jusfederal_fem", "4.1 .*sentenciados.* aberto.*Federal Femin") %>%
+  renomeia_coluna("quantidade_sentenca_ra_outrajus_mas", "4.1 .*sentenciados.* aberto.*Outros.* Masc") %>%
+  renomeia_coluna("quantidade_sentenca_ra_outrajus_fem", "4.1 .*sentenciados.* aberto.*Outros.* Femin") %>%
+  # presos em medida de segurança - internação
+  renomeia_coluna("quantidade_medseg_internacao_jusestadual_mas", "4.1 .*de segurança.* interna.*Estadual Masc") %>%
+  renomeia_coluna("quantidade_medseg_internacao_jusestadual_fem", "4.1 .*de segurança.* interna.*Estadual Femin") %>%
+  renomeia_coluna("quantidade_medseg_internacao_jusfederal_mas", "4.1 .*de segurança.* interna.*Federal Masc") %>%
+  renomeia_coluna("quantidade_medseg_internacao_jusfederal_fem", "4.1 .*de segurança.* interna.*Federal Femin") %>%
+  renomeia_coluna("quantidade_medseg_internacao_outrajus_mas", "4.1 .*de segurança.* interna.*Outros.* Masc") %>%
+  renomeia_coluna("quantidade_medseg_internacao_outrajus_fem", "4.1 .*de segurança.* interna.*Outros.* Femin") %>%
+  # presos em medida de segurança - tratamento ambulatorial
+  renomeia_coluna("quantidade_medseg_ambulatorial_jusestadual_mas", "4.1 .*de segurança.* ambulatorial.*Estadual Masc") %>%
+  renomeia_coluna("quantidade_medseg_ambulatorial_jusestadual_fem", "4.1 .*de segurança.* ambulatorial.*Estadual Femin") %>%
+  renomeia_coluna("quantidade_medseg_ambulatorial_jusfederal_mas", "4.1 .*de segurança.* ambulatorial.*Federal Masc") %>%
+  renomeia_coluna("quantidade_medseg_ambulatorial_jusfederal_fem", "4.1 .*de segurança.* ambulatorial.*Federal Femin") %>%
+  renomeia_coluna("quantidade_medseg_ambulatorial_outrajus_mas", "4.1 .*de segurança.* ambulatorial.*Outros.* Masc") %>%
+  renomeia_coluna("quantidade_medseg_ambulatorial_outrajus_fem", "4.1 .*de segurança.* ambulatorial.*Outros.* Femin") #%>%
+  # # presos em RDD
+  # renomeia_coluna("quantidade_presos_rdd", "4.1 .*regime disciplinar diferenciado") %>%
+  # # estabelecimento - controle se provisórios estão a mais de 90 dias
+  # renomeia_coluna("controle_excesso_prazo_existe", "4.2 .*controle.*90 dias.*\\?$") %>%
+  # renomeia_coluna("quantidade_excesso_prazo_mas", "4.2 .*controle.*90 dias.*Masc") %>%
+  # renomeia_coluna("quantidade_excesso_prazo_fem", "4.2 .*controle.*90 dias.*Fem") %>%
+  #
+
 
 
 base_renomeada %>%
